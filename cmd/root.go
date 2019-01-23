750f3f8be9350a07426f668d9e1978d360934efb
Manuel de la Peña <manu@wedeploy.com>
2019-01-23 15:33:37 +0100

    Use root command as default for starting a web server exposing a REST API to check any plate
    The idea is to front the HTML returned by the DGT service with a REST API, so you could curl this new service to retrieve a JSON object with the result of the query

--- cmd/root.go
+++ cmd/root.go
@@ -2,17 +2,18 @@ package cmd
 
 import (
 	"log"
+	"net/http"
 
+	"github.com/gin-gonic/gin"
 	"github.com/spf13/cobra"
 )
 
 var rootCmd = &cobra.Command{
 	Use:   "dgt",
-	Short: "dgt allows you to gather information about Spanish car plates",
-	Long: `A Fast and Flexible CLI for gathering Spain's car plates for Eco stickers,
-				built with love by mdelapenya and friends in Go.`,
+	Short: "Inits a web server with a REST API to fetch the sticker for a given plate",
+	Long:  `Inits a web server with a REST API to fetch the sticker for a given plate`,
 	Run: func(cmd *cobra.Command, args []string) {
-		//
+		runServer()
 	},
 }
 
@@ -21,4 +22,28 @@ func Execute() {
 	if err := rootCmd.Execute(); err != nil {
 		log.Fatalf("Failed!")
 	}
+}
+
+func fetchPlate(c *gin.Context) {
+	plate := c.Param("plate")
+
+	sticker := processPlate(plate)
+
+	status := http.StatusOK
+
+	if sticker == "Not found" || sticker == notFound {
+		status = http.StatusNotFound
+	}
+
+	c.JSON(status, gin.H{
+		"result": sticker,
+	})
+}
+
+func runServer() {
+	router := gin.Default()
+
+	router.GET("/plates/:plate", fetchPlate)
+
+	router.Run()
 }
