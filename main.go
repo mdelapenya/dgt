976d683b753e4f6953a96ad6bbebe81f314636e5
Manuel de la Peña <manu@wedeploy.com>
2019-01-23 03:03:30 +0100

    Initial commit

---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/mdelapenya/dgt/cmd"
+
+func main() {
+	cmd.Execute()
+}
---
+++ parser/html.go
@@ -0,0 +1,20 @@
+package parser
+
+import (
+	"log"
+	"strings"
+
+	htmlquery "github.com/antchfx/htmlquery"
+)
+
+// Parse parses an HTML
+func Parse(HTML string) string {
+	doc, err := htmlquery.Parse(strings.NewReader(HTML))
+	if err != nil {
+		log.Fatalln("The HTML is not parseable")
+	}
+
+	result := htmlquery.FindOne(doc, `//div[@id="resultadoBusqueda"]/div/p/text()`)
+
+	return result.Data
+}
