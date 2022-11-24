5a520524cb3dd7adebb7f1baa528c9c666a85f03
Manuel de la Peña <mdelapenya@gmail.com>
2022-11-24 07:24:00 +0100

    chore: use fmt's built-in format options

--- internal/format.go
+++ internal/format.go
@@ -3,17 +3,5 @@ package internal
 import "fmt"
 
 func FormatNumber(n int) string {
-	if n < 10 {
-		return fmt.Sprintf("000%d", n)
-	}
-
-	if n < 100 {
-		return fmt.Sprintf("00%d", n)
-	}
-
-	if n < 1000 {
-		return fmt.Sprintf("0%d", n)
-	}
-
-	return fmt.Sprintf("%d", n)
+	return fmt.Sprintf("%04d", n)
 }
