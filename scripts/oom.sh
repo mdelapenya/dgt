35af2f66b978d31e4f34310ded8f88e7469a1d88
Manuel de la Peña <mdelapenya@gmail.com>
2025-12-11 11:45:11 +0100

    chore: incorporate N to the scripts

--- scripts/oom.sh
+++ scripts/oom.sh
@@ -15,5 +15,6 @@ docker exec -it dgt-db-1 mysql -u root -ppassw0rd --database=dgt -e "select plat
 docker exec -it dgt-db-1 mysql -u root -ppassw0rd --database=dgt -e "select plate from plates where plate_id=(select plate_id from plates where plate like '____K__' order by plate_id desc limit 1);" >> ./plates.txt
 docker exec -it dgt-db-1 mysql -u root -ppassw0rd --database=dgt -e "select plate from plates where plate_id=(select plate_id from plates where plate like '____L__' order by plate_id desc limit 1);" >> ./plates.txt
 docker exec -it dgt-db-1 mysql -u root -ppassw0rd --database=dgt -e "select plate from plates where plate_id=(select plate_id from plates where plate like '____M__' order by plate_id desc limit 1);" >> ./plates.txt
+docker exec -it dgt-db-1 mysql -u root -ppassw0rd --database=dgt -e "select plate from plates where plate_id=(select plate_id from plates where plate like '____N__' order by plate_id desc limit 1);" >> ./plates.txt
 
 echo $(cat ./plates.txt | grep -v "mysql" | grep -v '-' | grep -v 'plate' | grep -v '=' | grep -v 'PLATE') > ./plates.txt
